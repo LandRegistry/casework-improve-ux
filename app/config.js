91b793df949563b765fd1d6a55ad894679eae6dd
Chanoch Wiggers <chanoch.wiggers@landregistry.gov.uk>
2018-05-16 16:01:43 +0100

    Add banner as include.

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Land Registry',
+  serviceName: 'HMLR Application to Change the Register',
 
   // Default port that prototype runs on
   port: '3000',
